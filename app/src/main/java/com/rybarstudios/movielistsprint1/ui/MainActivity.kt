151c9900f181c648d6dafc47856fd606b0efad64
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-08-09 11:16:40 -0400

    Added 2nd activity

---
+++ app/src/main/java/com/rybarstudios/movielistsprint1/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rybarstudios.movielistsprint1.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rybarstudios.movielistsprint1.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/rybarstudios/movielistsprint1/ui/MovieActivity.kt
+++ app/src/main/java/com/rybarstudios/movielistsprint1/ui/MovieActivity.kt
@@ -1,12 +1,13 @@
-package com.rybarstudios.movielistsprint1
+package com.rybarstudios.movielistsprint1.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rybarstudios.movielistsprint1.R
 
-class MainActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie)
     }
 }
