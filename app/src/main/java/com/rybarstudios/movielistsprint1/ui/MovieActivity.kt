96ecf277aa788e65629922aa0f6c8830cb7db6ec
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-08-09 11:40:27 -0400

    Added add movie layout

--- app/src/main/java/com/rybarstudios/movielistsprint1/ui/MovieActivity.kt
+++ app/src/main/java/com/rybarstudios/movielistsprint1/ui/MovieActivity.kt
@@ -9,5 +9,7 @@ class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
+
+        val intent = intent
     }
 }
